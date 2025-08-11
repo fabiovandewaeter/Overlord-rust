663447903c1acb610d66a8fe217e2350aa624d2b
Vandewaeter Fabio <vandewaeter.fabio@gmail.com>
2025-08-12 00:24:43 +0200

    add player

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,12 @@ struct UpsCounter {
     ups: u32,
 }
 
+#[derive(Component)]
+struct Entity {
+    movement_speed: f32,
+    rotation_speed: f32,
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -30,8 +36,8 @@ fn main() {
             last_second: 0.0,
             ups: 0,
         })
-        .insert_resource(Time::<Fixed>::from_seconds(1.0 / TARGET_UPS))
-        .add_systems(Update, (handle_inputs, update_render, display_fps_ups))
+        .insert_resource(Time::<Fixed>::from_hz(TARGET_UPS))
+        .add_systems(Update, (handle_camera_inputs, display_fps_ups))
         .add_systems(FixedUpdate, update_logic)
         .run();
 }
@@ -40,9 +46,18 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    window: Single<&Window>,
+    asset_server: Res<AssetServer>,
 ) {
     use bevy::color::palettes::css::GREEN;
+
+    let player_texture_handle = asset_server.load("default.png");
+    commands.spawn((
+        Sprite::from_image(player_texture_handle),
+        Entity {
+            movement_speed: 500.0,
+            rotation_speed: f32::to_radians(360.0),
+        },
+    ));
 
     commands.spawn((Camera2d, Camera { ..default() }));
     commands.spawn((
@@ -51,7 +66,7 @@ fn setup(
     ));
 }
 
-fn handle_inputs(
+fn handle_camera_inputs(
     mut camera_query: Query<(&mut Camera, &mut Transform, &mut Projection)>,
     input: Res<ButtonInput<KeyCode>>,
     mut input_mouse_wheel: EventReader<MouseWheel>,
@@ -115,14 +130,50 @@ fn handle_inputs(
     }
 }
 
-fn update_logic(mut counter: ResMut<UpsCounter>, time: Res<Time>) {
+const BOUNDS: Vec2 = Vec2::new(1200.0, 640.0);
+
+fn update_logic(
+    mut counter: ResMut<UpsCounter>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    query: Single<(&Entity, &mut Transform)>,
+    time: Res<Time>,
+) {
     counter.ticks += 1;
 
-    // Exemple de logique : déplacer un truc, calculer une simulation...
-}
+    let (ship, mut transform) = query.into_inner();
 
-fn update_render() {
-    // Rendu visuel à chaque frame
+    let mut rotation_factor = 0.0;
+    let mut movement_factor = 0.0;
+
+    if keyboard_input.pressed(KeyCode::ArrowLeft) {
+        rotation_factor += 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowRight) {
+        rotation_factor -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowUp) {
+        movement_factor += 1.0;
+    }
+
+    // Update the ship rotation around the Z axis (perpendicular to the 2D plane of the screen)
+    transform.rotate_z(rotation_factor * ship.rotation_speed * time.delta_secs());
+
+    // Get the ship's forward vector by applying the current rotation to the ships initial facing
+    // vector
+    let movement_direction = transform.rotation * Vec3::Y;
+    // Get the distance the ship will move based on direction, the ship's movement speed and delta
+    // time
+    let movement_distance = movement_factor * ship.movement_speed * time.delta_secs();
+    // Create the change in translation using the new movement direction and distance
+    let translation_delta = movement_direction * movement_distance;
+    // Update the ship translation with our new translation delta
+    transform.translation += translation_delta;
+
+    // Bound the ship within the invisible level bounds
+    let extents = Vec3::from((BOUNDS / 2.0, 0.0));
+    transform.translation = transform.translation.min(extents).max(-extents);
 }
 
 fn display_fps_ups(
