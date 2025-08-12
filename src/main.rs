4dc977e5c91e8f8aac1fed4f7cd91572e089d126
Vandewaeter Fabio <vandewaeter.fabio@gmail.com>
2025-08-12 17:35:34 +0200

    update: add tilemap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use bevy::diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin};
-use bevy::input::mouse::{MouseScrollUnit, MouseWheel};
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
+    input::mouse::{MouseScrollUnit, MouseWheel},
+    prelude::*,
+};
+use rand::Rng;
+
+use crate::map::{ChunkManager, MapPlugin, camera_pos_to_chunk_pos, spawn_chunk};
+
+mod map;
 
 const TARGET_UPS: f64 = 30.0;
 const ZOOM_IN_SPEED: f32 = 0.25 / 400000000.0;
@@ -14,32 +21,9 @@ struct UpsCounter {
 }
 
 #[derive(Component)]
-struct Entity {
+struct Unit {
     movement_speed: f32,
     rotation_speed: f32,
-}
-
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Overlord".to_string(),
-                present_mode: bevy::window::PresentMode::AutoVsync,
-                ..default()
-            }),
-            ..default()
-        }))
-        .add_plugins(FrameTimeDiagnosticsPlugin::default())
-        .add_systems(Startup, setup)
-        .insert_resource(UpsCounter {
-            ticks: 0,
-            last_second: 0.0,
-            ups: 0,
-        })
-        .insert_resource(Time::<Fixed>::from_hz(TARGET_UPS))
-        .add_systems(Update, (handle_camera_inputs, display_fps_ups))
-        .add_systems(FixedUpdate, update_logic)
-        .run();
 }
 
 fn setup(
@@ -51,13 +35,19 @@ fn setup(
     use bevy::color::palettes::css::GREEN;
 
     let player_texture_handle = asset_server.load("default.png");
-    commands.spawn((
-        Sprite::from_image(player_texture_handle),
-        Entity {
-            movement_speed: 500.0,
-            rotation_speed: f32::to_radians(360.0),
-        },
-    ));
+    let mut rng = rand::rng();
+    for i in 0..10 {
+        let random_number: i32 = rng.gen_range(0..500); // un entier de 0 à 9
+
+        commands.spawn((
+            Sprite::from_image(player_texture_handle.clone()),
+            Unit {
+                // movement_speed: 500.0,
+                movement_speed: random_number as f32,
+                rotation_speed: f32::to_radians(360.0),
+            },
+        ));
+    }
 
     commands.spawn((Camera2d, Camera { ..default() }));
     commands.spawn((
@@ -104,10 +94,6 @@ fn handle_camera_inputs(
             use bevy::math::ops::powf;
             match mouse_wheel_event.unit {
                 MouseScrollUnit::Line => {
-                    println!(
-                        "Scroll (line units): vertical: {}, horizontal: {}",
-                        mouse_wheel_event.y, mouse_wheel_event.x
-                    );
                     if mouse_wheel_event.y > 0.0 {
                         projection2d.scale *= powf(ZOOM_IN_SPEED, time.delta_secs());
                     } else if mouse_wheel_event.y < 0.0 {
@@ -115,10 +101,6 @@ fn handle_camera_inputs(
                     }
                 }
                 MouseScrollUnit::Pixel => {
-                    println!(
-                        "Scroll (pixel units): vertical: {}, horizontal: {}",
-                        mouse_wheel_event.y, mouse_wheel_event.x
-                    );
                     if mouse_wheel_event.y > 0.0 {
                         projection2d.scale *= powf(ZOOM_IN_SPEED, time.delta_secs());
                     } else if mouse_wheel_event.y < 0.0 {
@@ -130,50 +112,48 @@ fn handle_camera_inputs(
     }
 }
 
-const BOUNDS: Vec2 = Vec2::new(1200.0, 640.0);
-
 fn update_logic(
     mut counter: ResMut<UpsCounter>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    query: Single<(&Entity, &mut Transform)>,
+    // query: Single<(&Entity, &mut Transform)>,
+    query: Query<(&Unit, &mut Transform)>,
     time: Res<Time>,
 ) {
     counter.ticks += 1;
 
-    let (ship, mut transform) = query.into_inner();
+    for entity in query {
+        // let (ship, mut transform) = query.into_inner();
+        let (ship, mut transform) = entity;
 
-    let mut rotation_factor = 0.0;
-    let mut movement_factor = 0.0;
+        let mut rotation_factor = 0.0;
+        let mut movement_factor = 0.0;
 
-    if keyboard_input.pressed(KeyCode::ArrowLeft) {
-        rotation_factor += 1.0;
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            rotation_factor += 1.0;
+        }
+
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            rotation_factor -= 1.0;
+        }
+
+        if keyboard_input.pressed(KeyCode::ArrowUp) {
+            movement_factor += 1.0;
+        }
+
+        // Update the ship rotation around the Z axis (perpendicular to the 2D plane of the screen)
+        transform.rotate_z(rotation_factor * ship.rotation_speed * time.delta_secs());
+
+        // Get the ship's forward vector by applying the current rotation to the ships initial facing
+        // vector
+        let movement_direction = transform.rotation * Vec3::Y;
+        // Get the distance the ship will move based on direction, the ship's movement speed and delta
+        // time
+        let movement_distance = movement_factor * ship.movement_speed * time.delta_secs();
+        // Create the change in translation using the new movement direction and distance
+        let translation_delta = movement_direction * movement_distance;
+        // Update the ship translation with our new translation delta
+        transform.translation += translation_delta;
     }
-
-    if keyboard_input.pressed(KeyCode::ArrowRight) {
-        rotation_factor -= 1.0;
-    }
-
-    if keyboard_input.pressed(KeyCode::ArrowUp) {
-        movement_factor += 1.0;
-    }
-
-    // Update the ship rotation around the Z axis (perpendicular to the 2D plane of the screen)
-    transform.rotate_z(rotation_factor * ship.rotation_speed * time.delta_secs());
-
-    // Get the ship's forward vector by applying the current rotation to the ships initial facing
-    // vector
-    let movement_direction = transform.rotation * Vec3::Y;
-    // Get the distance the ship will move based on direction, the ship's movement speed and delta
-    // time
-    let movement_distance = movement_factor * ship.movement_speed * time.delta_secs();
-    // Create the change in translation using the new movement direction and distance
-    let translation_delta = movement_direction * movement_distance;
-    // Update the ship translation with our new translation delta
-    transform.translation += translation_delta;
-
-    // Bound the ship within the invisible level bounds
-    let extents = Vec3::from((BOUNDS / 2.0, 0.0));
-    transform.translation = transform.translation.min(extents).max(-extents);
 }
 
 fn display_fps_ups(
@@ -195,4 +175,49 @@ fn display_fps_ups(
             }
         }
     }
+}
+
+fn spawn_chunks_around_units(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    camera_query: Query<&Transform, With<Camera>>,
+    units_query: Query<&Transform, With<Unit>>,
+    mut chunk_manager: ResMut<ChunkManager>,
+) {
+    // for transform in camera_query.iter() {
+    for unit_transform in units_query {
+        let camera_chunk_pos = camera_pos_to_chunk_pos(&unit_transform.translation.xy());
+        for y in (camera_chunk_pos.y - 2)..(camera_chunk_pos.y + 2) {
+            for x in (camera_chunk_pos.x - 2)..(camera_chunk_pos.x + 2) {
+                if !chunk_manager.spawned_chunks.contains(&IVec2::new(x, y)) {
+                    chunk_manager.spawned_chunks.insert(IVec2::new(x, y));
+                    spawn_chunk(&mut commands, &asset_server, IVec2::new(x, y));
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Overlord".to_string(),
+                present_mode: bevy::window::PresentMode::AutoVsync,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(FrameTimeDiagnosticsPlugin::default())
+        .add_plugins(MapPlugin)
+        .insert_resource(UpsCounter {
+            ticks: 0,
+            last_second: 0.0,
+            ups: 0,
+        })
+        .insert_resource(Time::<Fixed>::from_hz(TARGET_UPS))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (handle_camera_inputs, display_fps_ups))
+        .add_systems(FixedUpdate, (update_logic, spawn_chunks_around_units))
+        .run();
 }
